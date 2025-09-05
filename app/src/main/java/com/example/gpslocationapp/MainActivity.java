2d16b5abf992729aaa93e674d1f187a6bb7e553f
Mitchell Travis <Kiwitiendatm@gmail.com>
2025-09-05 14:35:29 +1200

    xml file added

--- app/src/main/java/com/example/gpslocationapp/MainActivity.java
+++ app/src/main/java/com/example/gpslocationapp/MainActivity.java
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+//Location permissions added to AndroidManifest.xml
 public class MainActivity extends AppCompatActivity {
 
     @Override
